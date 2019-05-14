9b793bca3c4bb34c7513317587959a053299c75c
manage <manage@163.com>
2019-05-14 17:18:32 +0800

    05-qqlogintool的使用

--- meiduo_mall02/apps/oauto/views.py
+++ meiduo_mall02/apps/oauto/views.py
@@ -9,7 +9,11 @@ from utils.response_code import RETCODE
 class QQAuthURLView(View):
 
     def get(self,request):
-        #接受请求对象,从那个
+        # 普通方式qq登录页面的网址
+        #login_url = 'https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=101518219&redirect_uri=http://www.meiduo.site:8000/oauth_callback&state=400'
+
+        #动态方式拼接
+        #接受请求对象,从那个网址进入的，将来成功登入之后回调的地址就是那个
         next = request.GET.get('next')
         #准备qq登录页面的网址
         oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
@@ -19,7 +23,7 @@ class QQAuthURLView(View):
                         )
         #拼接qq登录的路由
         login_url = oauth.get_qq_url()
-        #返回相应对象
+        # 返回相应对象
         return http.JsonResponse({'code':RETCODE.OK,
                                   'errmsg':'OK',
                                   'login_url':login_url
