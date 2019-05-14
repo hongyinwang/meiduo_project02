84bcef4fa7cc636a0af480ae0daa1b75c850a9a4
manage <manage@163.com>
2019-05-14 14:49:30 +0800

    定义QQ登录模型类

---
+++ meiduo_mall02/apps/oauto/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OautoConfig(AppConfig):
+    name = 'oauto'
--- meiduo_mall02/meiduo_mall02/settings.py
+++ meiduo_mall02/meiduo_mall02/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.users',
     'apps.contents',
-    'apps.varifications'
+    'apps.varifications',
+    'apps.oauto'
 ]
 
 MIDDLEWARE = [
